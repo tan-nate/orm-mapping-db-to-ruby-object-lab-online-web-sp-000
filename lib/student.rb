b13d077ed16be4e615aab59f23ee86d1e8893aaf
Nate Tan <nktan93@gmail.com>
2019-05-07 22:27:51 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,13 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    
-  end
+  new_song = self.new  # self.new is the same as running Song.new
+  new_song.id = row[0]
+  new_song.name =  row[1]
+  new_song.length = row[2]
+  new_song  # return the newly created instance
+end
+  
 
   def self.all
     # retrieve all the rows from the "Students" database
